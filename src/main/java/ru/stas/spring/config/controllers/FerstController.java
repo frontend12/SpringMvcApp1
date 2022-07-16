78d7a78177fadb1d9bd1ede4e6cb610da01ba548
frontend12 <stasstykov@gmail.com>
2022-07-16 18:21:35 +0300

    Th

--- src/main/java/ru/stas/spring/config/controllers/FerstController.java
+++ src/main/java/ru/stas/spring/config/controllers/FerstController.java
@@ -2,6 +2,7 @@ package ru.stas.spring.config.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +15,12 @@ public class FerstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname){
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
 
 
-        System.out.println("Hello," + name + " " +surname);
+        //System.out.println("Hello," + name + " " +surname);
+        model.addAttribute("message","Hello," + name + " " +surname);
 
         return "first/hello";
 
@@ -28,5 +31,31 @@ public class FerstController {
     public String goodByePage(){
         return "first/goodbye";
 
+    }
+
+    @GetMapping("/colculator")
+    public String colculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model){
+        double result;
+
+        switch (action){
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case  "addition":
+                result = a+b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("result",result);
+        return "first/colculator";
     }
 }
